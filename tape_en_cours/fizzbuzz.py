5c0c6a94a19aaae87b5c87776741b035dd9f21a4
Matthieu Falce <falce.matthieu@gmail.com>
2023-01-10 16:46:39 +0100

    gitautosync update 27

--- tape_en_cours/fizzbuzz.py
+++ tape_en_cours/fizzbuzz.py
@@ -1,10 +1,10 @@
 def regle_fizz_buzz(nombre):
     res = nombre
-    if res % 3 == 0 and res % 5 == 0:
-        res = "fizzbuzz"
+    if res % 3 == 0:
+        res = "buzz"
     elif res % 3 == 0:
         res = "fizz"
-    elif res % 5 == 0:
-        res = "buzz"
+    elif res % 5 == 0 and res % 3 == 0:
+        res = "fizzbuzz"
 
     return res
