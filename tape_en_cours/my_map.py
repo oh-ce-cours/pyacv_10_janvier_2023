063d2f27c2cd76f095c72cb0df2ea1df151d392b
Matthieu Falce <falce.matthieu@gmail.com>
2023-01-11 10:58:52 +0100

    gitautosync update 88

--- tape_en_cours/my_map.py
+++ tape_en_cours/my_map.py
@@ -9,3 +9,18 @@ def my_map(f: Callable[[E], S], data: Iterator[E]) -> List[S]:
     for i in data:
         res.append(f(i))
     return res
+
+
+def my_lazy_map(
+    func: Callable[[T_in], T_out], iterable: Iterable[T_in]
+) -> Iterable[T_out]:
+    for item in iterable:
+        yield func(item)
+
+
+def my_filter(
+    func: Callable[[T_in], bool], iterable: Iterable[T_in]
+) -> Iterable[T_out]:
+    for item in iterable:
+        if func(item):
+            yield item
