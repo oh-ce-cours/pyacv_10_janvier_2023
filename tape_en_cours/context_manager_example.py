d4816e33c3ee808e1adf5ce9b8d37483705afaf4
Matthieu Falce <falce.matthieu@gmail.com>
2023-01-12 11:58:37 +0100

    gitautosync update 222

--- tape_en_cours/context_manager_example.py
+++ tape_en_cours/context_manager_example.py
@@ -14,6 +14,19 @@ class ChangeOutput:
         sys.stdout = self.old_stdout
 
 
+class ChangePrint:
+    def __init__(self):
+        self.old_stdout = sys.stdout
+        self.output_file = open("./stdout.txt", "w")
+
+    def __enter__(self):
+        sys.stdout = self.output_file
+
+    def __exit__(self, *args):
+        self.output_file.close()
+        sys.stdout = self.old_stdout
+
+
 print("tata")
 try:
     with ChangeOutput() as print:
