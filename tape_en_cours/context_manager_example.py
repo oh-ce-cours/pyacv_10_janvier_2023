baf15ef237b2e465531e56bf617c8b1c3fc5ce8f
Matthieu Falce <falce.matthieu@gmail.com>
2023-01-12 11:55:03 +0100

    gitautosync update 215

--- tape_en_cours/context_manager_example.py
+++ tape_en_cours/context_manager_example.py
@@ -1,5 +1,15 @@
 import sys
 
-old_stdout = sys.stdout
-sys.stdout = open("./stdout.txt", "w")
-print("tata\n")
+
+class ChangeOutput:
+    def __init__(self):
+        self.old_stdout = sys.stdout
+        self.output_file = open("./stdout.txt", "w")
+        sys.stdout = self.output_file
+
+    def __exit__(self, *args):
+        self.output_file.close()
+        sys.stdout = self.old_stdout
+
+
+print("tata")
