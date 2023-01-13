ed85a543e896170c7c86bf584e2d474dbd641f4c
Matthieu Falce <falce.matthieu@gmail.com>
2023-01-13 15:10:13 +0100

    gitautosync update 374

--- tape_en_cours/test_package/setup.py
+++ tape_en_cours/test_package/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="funniest",
-    version="0.1",
+    version="0.2",
     description="Super blague",
     url="http://example.com/ahahah/",
     author="Matthieu Falce",
