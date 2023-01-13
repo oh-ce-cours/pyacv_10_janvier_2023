456d48fd75fc5c73b516779e9e0ed8619396bd20
Matthieu Falce <falce.matthieu@gmail.com>
2023-01-13 12:03:40 +0100

    gitautosync update 338

--- tape_en_cours/my_pathlib.py
+++ tape_en_cours/my_pathlib.py
@@ -52,7 +52,7 @@ class WindowsPath(AbstractPath):
 
 
 if __name__ == "__main__":
-    p = AbstractPath.create()
+    p = AbstractPath()
     p1 = p / "etc"
     p2 = p1 / "nginx"
     print(id(p1), id(p2))
