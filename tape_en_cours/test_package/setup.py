978bc43ffb7bea89f352d9ce1052f518c89aea32
Matthieu Falce <falce.matthieu@gmail.com>
2023-01-13 14:58:24 +0100

    gitautosync update 365

---
+++ tape_en_cours/test_package/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="funniest",
+    version="0.1",
+    description="Super blague",
+    url="http://example.com/ahahah/",
+    author="Matthieu Falce",
+    author_email="clown@example.com",
+    license="MIT",
+    packages=["funniest"],
+    zip_safe=False,
+)
