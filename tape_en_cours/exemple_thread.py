c545ca186197df5ab49f55993d82149bc69935ee
Matthieu Falce <falce.matthieu@gmail.com>
2023-01-13 14:28:26 +0100

    gitautosync update 347

--- tape_en_cours/exemple_thread.py
+++ tape_en_cours/exemple_thread.py
@@ -1,20 +1,33 @@
 from threading import Thread
 import time
+
 # Define a function for the thread
 def print_time(threadName, delay):
     count = 0
     while count < 5:
-    time.sleep(delay)
-    count += 1
-    print("{}, {}".format(
-    threadName,
-    time.ctime(time.time())
-    ))
+        time.sleep(delay)
+        count += 1
+        print("{}, {}".format(threadName, time.ctime(time.time())))
+
 
 # Create two threads as follows
-t1 = Thread(target=print_time, args=("Thread-1", 0.1,))
-t2 = Thread(target=print_time, args=("Thread-2", 0.2,))
-t1.start(); t2.start()
+t1 = Thread(
+    target=print_time,
+    args=(
+        "Thread-1",
+        0.1,
+    ),
+)
+t2 = Thread(
+    target=print_time,
+    args=(
+        "Thread-2",
+        0.2,
+    ),
+)
+t1.start()
+t2.start()
 print("Pas bloqué")
-t1.join(); t2.join()
+t1.join()
+t2.join()
 print("Fini")
